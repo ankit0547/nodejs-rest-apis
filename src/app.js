a7bfa0ae3c3b9057269ce5850cfc47120aa947c4
Ankit sharma <ankit.sharma09547@gmail.com>
2024-09-20 01:35:21 +0530

    env update

--- src/app.js
+++ src/app.js
@@ -54,7 +54,7 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "16kb" }));
+app.use(express.json({ limit: "16kb", strict: false }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public")); // configure static file to save images locally
 app.use(cookieParser());
