2e5df1287c7e259d48ea9a9f2d9998c96c71f903
Ankit Sharma <ankit.sharma09547@gmail.com>
2025-02-12 17:08:56 +0530

    health check path updated

--- src/app.js
+++ src/app.js
@@ -78,7 +78,7 @@ import userRouter from './route/user/user.routes.js';
 import authRouter from './route/auth/auth.routes.js';
 
 // * healthcheck
-app.use('/', healthcheckRouter);
+app.use('/api', healthcheckRouter);
 
 // * App apis
 app.use('/api/v1/user', userRouter);
